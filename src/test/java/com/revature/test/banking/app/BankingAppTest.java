62c1892c9593b37327f91d32f14d152555d8648e
Chris Rougraff <croug327@gmail.com>
2018-08-13 16:08:57 -0400

    added another test case

--- src/test/java/com/revature/test/banking/app/BankingAppTest.java
+++ src/test/java/com/revature/test/banking/app/BankingAppTest.java
@@ -23,10 +23,17 @@ public class BankingAppTest {
 		assertEquals(ud.findByUsername(currentUser.getUsername()).getUsername(), currentUser.getUsername());
 	}
 	@Test
-	public void testDeposit() {
+	public void testUserDeposit() {
 		User currentUser2 = new User("testUsername2", "testPassword2","testFirst","testLast",21, 0, new HashMap<String, Double>());
 		currentUser2.deposit(100);
 		ud.createUser(currentUser2);
 		assertEquals(ud.findByUsername(currentUser2.getUsername()).getBalance(), currentUser2.getBalance(),0);
 	}	
+	@Test
+	public void testUserWithdraw() {
+		User currentUser3 = new User("testUsername3", "testPassword3","testFirst","testLast",21, 100, new HashMap<String, Double>());
+		currentUser3.withdraw(100);
+		ud.createUser(currentUser3);
+		assertEquals(ud.findByUsername(currentUser3.getUsername()).getBalance(), currentUser3.getBalance(),0);
+	}
 }
